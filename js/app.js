1a3dd2e1886b2078ca74906e775f78a023729aa4
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2019-04-01 23:03:31 -0500

    Added reset function

--- js/app.js
+++ js/app.js
@@ -11,16 +11,17 @@ const phrases = [
   'break a leg',
   'back to square one',
   'see eye to eye',
-  'a piece of cake'
+  'a piece of cake',
+  'two birds with one stone'
 ];
+const buttons = document.getElementsByTagName('button');
 const letter = document.getElementsByClassName('letter');
 const show = document.getElementsByClassName('show');
+const ul = document.getElementsByTagName('ul')[0];
 
-start.addEventListener('click', () => {
-  overlay.style.display = 'none';
-  // hides the start screen overlay
-});
+// ------------------------------------------------------------------- //
 
+// FUNCTIONS
 const getRandomPhraseAsArray = (arr) => {
   let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
   return randomPhrase.split('');
@@ -47,6 +48,7 @@ addPhraseToDisplay(phraseArray);
 // appends to game display
 
 const checkLetter = (button) => {
+  // checks for letter matches
 	let match = null;
 	for (let i = 0; i < letter.length; i++) {
 	    if (button.textContent === letter[i].textContent) {
@@ -57,43 +59,74 @@ const checkLetter = (button) => {
 	return match;
 }
 
+const reset = () => {
+  // resets the game board
+  start.addEventListener('click', () => {
+    overlay.classList.remove = 'win';
+    overlay.classList.remove = 'lose';
+    overlay.style.display = 'none';
+  });
+  missed = 0;
+  const board = document.querySelectorAll('.space, .letter');
+  for (let i = 0; i < board.length; i++) {
+    ul.removeChild(board[i]);
+  }
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].className = "";
+    buttons[i].disabled = false;
+  }
+  const phraseArray = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phraseArray); 
+  hearts.forEach((img) => {
+    // resets the hearts
+    img.src = 'images/liveHeart.png';
+  });
+}
+
 const checkWin = () => {
+  // checks to see if user key matches the correct letter
   if (show.length === letter.length) {
+    setTimeout(() => {
     title.textContent = 'You are a winner!';
-		start.textContent = 'Start a new game!';
+		start.textContent = 'Play Again!';
 		overlay.style.display = 'flex';
     overlay.classList.add('win');
     console.log('winner');
+    reset();
+    }, 1300);
     } else if (missed >= 5) {
-        title.textContent = 'Try again next time!';
+        setTimeout(() => {
+        title.textContent = 'You Lose! Try again!';
         start.textContent = 'Start a new game!';
         overlay.style.display = 'flex';
         overlay.classList.add('lose');
         console.log('loser');
-  }
-}
+        reset();
+        }, 1300);
+      }
+    }
+
+// ------------------------------------------------------------------- //
+
+// BEGIN GAME
+start.addEventListener('click', () => {
+  overlay.style.display = 'none';
+  // hides the start screen overlay
+});
 
 qwerty.addEventListener('click', (e) => {
+// when user clicks a key to match with phrase
   if (e.target.tagName === 'BUTTON') {
     e.target.classList.add('chosen');
     e.target.disabled = true; 
-  }
-  let letterFound = checkLetter(e.target);
+    let letterFound = checkLetter(e.target);
   if (letterFound) {
     checkWin();
-    start.addEventListener('click', () => {
-      window.location.reload();
-    });
   } else if (!letterFound) {
       missed += 1;
       let lostHearts = document.querySelectorAll(".tries img")[missed - 1];
       lostHearts.src = "images/lostHeart.png";
       checkWin();
-      start.addEventListener('click', () => {
-        window.location.reload();
-      });
     }
+  }
 });
-
-
-
